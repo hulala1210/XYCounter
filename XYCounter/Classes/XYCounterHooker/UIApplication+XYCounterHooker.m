938b182ef7de0b23029978cf8b89520cb7709c00
谌彦霖 <chenyanlin@appscomm.cn>
2020-12-28 15:54:00 +0800

    [Modify]修改UISlider的特殊判断。

--- XYCounter/Classes/XYCounterHooker/UIApplication+XYCounterHooker.m
+++ XYCounter/Classes/XYCounterHooker/UIApplication+XYCounterHooker.m
@@ -13,21 +13,22 @@
 @implementation UIApplication (XYCounterHooker)
 
 - (BOOL)xyCounter_sendAction:(SEL)action to:(nullable id)target from:(nullable id)sender forEvent:(nullable UIEvent *)event {
-    if ([sender isKindOfClass:UISwitch.class] || [sender isKindOfClass:UISegmentedControl.class] || [sender isKindOfClass:UIStepper.class] || event.allTouches.anyObject.phase == UITouchPhaseEnded) {
-        
-        XYCounterControlEvent *controlEvent = [[XYCounterControlEvent alloc] init];
-        controlEvent.sender = sender;
-        controlEvent.target = target;
-        controlEvent.action = action;
-        
-        if ([sender respondsToSelector:@selector(xyCounter_getViewController)]) {
-            controlEvent.viewController = [sender xyCounter_getViewController];
-        }
-        
-        controlEvent.event = event;
-        [[XYCounterEventDistributor sharedInstance] handleControlEvent:controlEvent];
-
+    
+    if ([sender isKindOfClass:UISlider.class] && event.allTouches.anyObject.phase != UITouchPhaseEnded) {
+        return [self xyCounter_sendAction:action to:target from:sender forEvent:event];
     }
+    
+    XYCounterControlEvent *controlEvent = [[XYCounterControlEvent alloc] init];
+    controlEvent.sender = sender;
+    controlEvent.target = target;
+    controlEvent.action = action;
+    
+    if ([sender respondsToSelector:@selector(xyCounter_getViewController)]) {
+        controlEvent.viewController = [sender xyCounter_getViewController];
+    }
+    
+    controlEvent.event = event;
+    [[XYCounterEventDistributor sharedInstance] handleControlEvent:controlEvent];
 
     return [self xyCounter_sendAction:action to:target from:sender forEvent:event];
 }
